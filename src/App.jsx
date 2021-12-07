d4564ae9c011dbe2bd4c6c2df060fabd7c7cafdf
vishal00923 <vishalbg.mz@gmail.com>
2021-12-07 18:48:36 +0530

    Add : App

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import HomePage from './pages/homepage/homepage.component';
+
+import './App.scss';
+
+const App = () => {
+    return (
+        <div>
+            <HomePage />
+        </div>
+    );
+};
+
+export default App;
