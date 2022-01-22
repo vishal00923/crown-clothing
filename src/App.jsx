2d9aadb85c03ca2eb60b6a079420baea2044b43d
vishal00923 <vishalbg.mz@gmail.com>
2022-01-23 02:46:24 +0530

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
 import './App.scss';
 
 const App = () => {
     return (
         <div>
-            <HomePage />
+            <Switch>
+                <Route exact path="/" component={HomePage} />
+                <Route path="/shop" component={ShopPage} />
+            </Switch>
         </div>
     );
 };
