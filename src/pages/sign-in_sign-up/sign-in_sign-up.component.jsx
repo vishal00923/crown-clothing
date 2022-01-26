54cc853a920f0a61b3bb00e6e3fbe740589b21e9
vishal00923 <vishalbg.mz@gmail.com>
2022-01-26 22:00:57 +0530

    Add : sign-in_sign-up page

---
+++ src/pages/sign-in_sign-up/sign-in_sign-up.component.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import SignIn from '../../components/sign-in/sign-in.component';
+
+import './sign-in_sign-up.styles.scss';
+
+const SignInSignUpPage = () => {
+    return (
+        <div className="sign-in_sign-up">
+            <SignIn />
+        </div>
+    );
+};
+
+export default SignInSignUpPage;
