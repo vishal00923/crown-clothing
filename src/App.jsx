eeb925b8682ffc0974262bf3da43ac9495601b28
Vishal Chaurasia <vishalbg.mz@gmail.com>
2022-05-04 00:53:57 +0530

    Update App Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,24 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
-import Header from './components/header/header.component';
-import HomePage from './pages/homepage/homepage.component';
-import ShopPage from './pages/shop/shop.component';
-import SignInSignUpPage from './pages/sign-in_sign-up/sign-in_sign-up.component';
+import Header from './Components/Header/Header';
+import HomePage from './Pages/HomePage/HomePage.';
+import ShopPage from './Pages/ShopPage/ShopPage';
+import SignInSignUpPage from './Pages/SignInSignUp/SignInSignUp';
 
 import './App.scss';
 
 const App = () => {
-    return (
-        <div className="app">
-            <Header />
-            <Switch>
-                <Route exact path="/" component={HomePage} />
-                <Route path="/shop" component={ShopPage} />
-                <Route path="/sign-in" component={SignInSignUpPage} />
-            </Switch>
-        </div>
-    );
+  return (
+    <div className="app">
+      <Header />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shop" component={ShopPage} />
+        <Route path="/sign-in" component={SignInSignUpPage} />
+      </Switch>
+    </div>
+  );
 };
 
 export default App;
