6071a80b8ea04f66995d142879234f8a885c2e7e
Vishal Chaurasia <vishalbg.mz@gmail.com>
2022-05-04 01:08:08 +0530

    Add CollectionItem Component

---
+++ src/Components/CollectionItem/CollectionItem.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './styles.scss';
+
+const CollectionItem = ({ id, name, price, imageUrl }) => {
+  return (
+    <div className="collection-item">
+      <div className="image" style={{ backgroundImage: `url(${imageUrl})` }} />
+      <div className="collection-footer">
+        <span className="name">{name}</span>
+        <span className="price">{price}</span>
+      </div>
+    </div>
+  );
+};
+
+export default CollectionItem;
