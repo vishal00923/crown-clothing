b16f0ca7b20209737cd7ecc75dcdc9657b8c1d42
Vishal Chaurasia <vishalbg.mz@gmail.com>
2022-05-04 00:36:15 +0530

    Delete Files

--- src/components/custom-button/custom-button.component.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import './custom-button.styles.scss';
-
-const CustomButton = ({ children }) => {
-    return <button className="custom-button">{children}</button>;
-};
-
-export default CustomButton;
