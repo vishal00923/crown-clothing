9b3063a0ce58694bae146e6dba169ddae4d7cc0f
Vishal Chaurasia <vishalbg.mz@gmail.com>
2022-05-04 01:03:22 +0530

    Add ShopPage Component

---
+++ src/Pages/ShopPage/ShopPage.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+
+import SHOP_DATA from './ShopData';
+import CollectionPreview from '../../Components/CollectionPreview/CollectionPreview';
+
+class ShopPage extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      collections: SHOP_DATA,
+    };
+  }
+
+  render() {
+    const { collections } = this.state;
+
+    return (
+      <div className="shop-page">
+        {collections.map((collection) => (
+          <CollectionPreview
+            key={collection.id}
+            title={collection.title}
+            items={collection.items}
+          />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default ShopPage;
