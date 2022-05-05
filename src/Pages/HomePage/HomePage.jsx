188743b225d8f51ec2361a9b3de6b1f27ec72f5f
Vishal Chaurasia <vishalbg.mz@gmail.com>
2022-05-05 16:21:03 +0530

    Add Components

---
+++ src/Pages/HomePage/HomePage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Directory from '../../Components/Directory/Directory';
+
+import './styles.scss';
+
+const HomePage = () => {
+  return (
+    <div className="homepage">
+      <Directory />
+    </div>
+  );
+};
+
+export default HomePage;
---
+++ src/Pages/SignInSignUpPage/SignInSignUpPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import SignIn from '../../Components/SignIn/SignIn';
+
+import './styles.scss';
+
+const SignInSignUpPage = () => {
+  return (
+    <div className="sign-in_sign-up">
+      <SignIn />
+    </div>
+  );
+};
+
+export default SignInSignUpPage;
