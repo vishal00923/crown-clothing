70c2cef74425a90d9b7c3bc957f6b2f2f46c27ae
Vishal Chaurasia <vishalbg.mz@gmail.com>
2022-05-15 19:25:28 +0530

    Upgrade react-router-dom ^6

--- src/Pages/SignInSignUpPage/SignInSignUpPage.jsx
+++ src/Pages/SignInSignUpPage/SignInSignUpPage.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 
 import SignIn from '../../Components/SignIn/SignIn';
 
-import './styles.scss';
-
 const SignInSignUpPage = () => {
   return (
     <div className="sign-in_sign-up">
