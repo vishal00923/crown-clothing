e13d98be9a0ff404cd31c407e5f02eab6e2866dc
Vishal Chaurasia <vishalbg.mz@gmail.com>
2022-09-12 22:19:55 +0530

    update SignInSignUpPage

--- src/Pages/SignInSignUpPage/SignInSignUpPage.jsx
+++ src/Pages/SignInSignUpPage/SignInSignUpPage.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import SignIn from '../../components/SignIn/SignIn';
+import SignUp from '../../components/SignUp/SignUp';
+
+import './styles.scss';
 
 export default function SignInSignUpPage() {
   return (
     <div className="sign-in_sign-up">
       <SignIn />
+      <SignUp />
     </div>
   );
 }
