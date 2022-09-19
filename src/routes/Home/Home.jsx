0f01c8837eeb929bd871f5ac1d11f0386cd195c9
Vishal Chaurasia <vishalbg.mz@gmail.com>
2022-09-19 12:11:23 +0530

    add Home route

---
+++ src/routes/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import Directory from '../../components/Directory/Directory';
+import './styles.scss';
+
+export default function Home() {
+  return (
+    <div className="home">
+      <Directory />
+    </div>
+  );
+}
