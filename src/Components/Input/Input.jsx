4b7a7515ce93eac0c27b157184aaaedb6654a18d
Vishal Chaurasia <vishalbg.mz@gmail.com>
2022-09-19 13:09:14 +0530

    add Input component

---
+++ src/Components/Input/Input.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import './styles.scss';
+
+export default function Input({
+  handleChange,
+  type,
+  name,
+  value,
+  label,
+  required,
+}) {
+  return (
+    <div className="input">
+      <input
+        className="inputFeild"
+        onChange={handleChange}
+        type={type}
+        name={name}
+        value={value}
+        required={required}
+      />
+
+      {label ? (
+        <label className={`${value.length ? 'shrink' : ''} label`}>
+          {label}
+        </label>
+      ) : null}
+    </div>
+  );
+}
