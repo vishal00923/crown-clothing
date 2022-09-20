5c1b1567e60965783948c851a0e661de7aefdca0
Vishal Chaurasia <vishalbg.mz@gmail.com>
2022-09-20 13:15:28 +0530

    add ProductCard component

---
+++ src/Components/ProductCard/ProductCard.jsx
@@ -0,0 +1,27 @@
+import React, { useContext } from 'react';
+import { CartContext } from '../../contexts/cartContext';
+
+import Button from '../Button/Button';
+import './styles.scss';
+
+export default function ProductCard({ product }) {
+  const { imageUrl, name, price } = product;
+  const { addItemToCart } = useContext(CartContext);
+
+  const handleAddItemToCart = () => {
+    addItemToCart(product);
+  };
+
+  return (
+    <div className="product">
+      <img src={imageUrl} alt={`${name}`} />
+      <div className="productFooter">
+        <span className="productName">{name}</span>
+        <span className="productPrice">{price}</span>
+      </div>
+      <Button handleClick={handleAddItemToCart} type="inverted">
+        add to cart
+      </Button>
+    </div>
+  );
+}
