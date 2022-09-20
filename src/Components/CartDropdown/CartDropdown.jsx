f2050b97ba37a39bb8b454216777bcea9ba4b9dd
Vishal Chaurasia <vishalbg.mz@gmail.com>
2022-09-20 20:06:58 +0530

    update CartDropdown component

--- src/Components/CartDropdown/CartDropdown.jsx
+++ src/Components/CartDropdown/CartDropdown.jsx
@@ -1,13 +1,33 @@
-import React from 'react';
-import CustomButton from '../CustomButton/CustomButton';
+import React, { useContext } from 'react';
+import { CartContext } from '../../contexts/cartContext';
+
+import Button from '../Button/Button';
+import CartItem from '../CartItem/CartItem';
+
+import { useNavigate } from 'react-router-dom';
 
 import './styles.scss';
 
 export default function CartDropdown() {
+  const { cartItems } = useContext(CartContext);
+  const navigate = useNavigate();
+
+  const handlegoToCheckout = () => {
+    navigate('/checkout');
+  };
+
   return (
-    <div className="cart-dropdown-container">
-      <div className="cart-items" />
-      <CustomButton>go to checkout</CustomButton>
+    <div className="cartDropdown">
+      <div className="cartDropdown__Items">
+        {cartItems.length ? (
+          cartItems.map((cartItem) => (
+            <CartItem key={cartItem.id} cartItem={cartItem} />
+          ))
+        ) : (
+          <p className="empty-message">Your cart is empty</p>
+        )}
+      </div>
+      <Button handleClick={handlegoToCheckout}>go to checkout</Button>
     </div>
   );
 }
