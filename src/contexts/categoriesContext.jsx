9bc4650a2c077e5bec3345bdefc3c8148909fd23
Vishal Chaurasia <vishalbg.mz@gmail.com>
2022-09-21 22:02:58 +0530

    add cartContext and categoriesContext

---
+++ src/contexts/categoriesContext.jsx
@@ -0,0 +1,27 @@
+import { createContext, useState, useEffect } from 'react';
+import { getCategoriesAndDocuments } from '../utils/firebase';
+
+export const CategoriesContext = createContext({
+  categoriesMap: {},
+});
+
+export const CategoriesProvider = ({ children }) => {
+  const [categoriesMap, setCategoriesMap] = useState({});
+
+  useEffect(() => {
+    const getCategoriesMap = async () => {
+      const categoriesMap = await getCategoriesAndDocuments();
+      setCategoriesMap(categoriesMap);
+    };
+
+    getCategoriesMap();
+  }, []);
+
+  const value = { categoriesMap };
+
+  return (
+    <CategoriesContext.Provider value={value}>
+      {children}
+    </CategoriesContext.Provider>
+  );
+};
