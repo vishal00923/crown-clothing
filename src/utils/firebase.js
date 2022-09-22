454679c8a0db180297fbf0a5a6b70bfafe97ea77
Vishal Chaurasia <vishalbg.mz@gmail.com>
2022-09-22 23:58:32 +0530

    update firebase utils

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,5 +1,14 @@
 import { initializeApp } from 'firebase/app';
-import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
+import {
+  getFirestore,
+  doc,
+  getDoc,
+  setDoc,
+  collection,
+  writeBatch,
+  query,
+  getDocs,
+} from 'firebase/firestore';
 import {
   getAuth,
   GoogleAuthProvider,
@@ -32,16 +41,41 @@ const signInWithGoogleRedirect = () => signInWithRedirect(auth, googleProvider);
 
 const db = getFirestore(firebaseApp);
 
+const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => {
+  const collectionRef = collection(db, collectionKey);
+  const batch = writeBatch(db);
+
+  objectsToAdd.forEach((object) => {
+    const docRef = doc(collectionRef, object.title.toLowerCase());
+    batch.set(docRef, object);
+  });
+
+  await batch.commit();
+  // console.log('batch commit successful');
+};
+
+const getCategoriesAndDocuments = async () => {
+  const collectionRef = collection(db, 'categories');
+  const q = query(collectionRef);
+
+  const querySnapshot = await getDocs(q);
+  const categoryMap = querySnapshot.docs.reduce((acc, docSnapshot) => {
+    const { title, items } = docSnapshot.data();
+    acc[title.toLowerCase()] = items;
+    return acc;
+  }, {});
+
+  return categoryMap;
+};
+
 const createUserDocumentFromAuth = async (userAuth, additionalInformation) => {
   if (!userAuth) return;
 
   // existing document reference
   const userDocRef = doc(db, 'users', userAuth.uid);
-  // console.log(userDocRef);
 
   // user data file object
   const userSnapshot = await getDoc(userDocRef);
-  // console.log(userSnapshot.exists());
 
   // if user exists
   if (!userSnapshot.exists()) {
@@ -88,4 +122,6 @@ export {
   createAuthUserWithEmailAndPassword,
   signOutUser,
   onAuthStateChangedListener,
+  addCollectionAndDocuments,
+  getCategoriesAndDocuments,
 };
