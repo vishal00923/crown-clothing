9c8d3e9564c6db5e03d58cb05f985e4736d9b0b0
Vishal Chaurasia <vishalbg.mz@gmail.com>
2022-09-22 23:58:02 +0530

    add assets

---
+++ src/data/categories.js
@@ -0,0 +1,36 @@
+const categories = [
+  {
+    id: 1,
+    title: 'hats',
+    imageUrl: 'https://bit.ly/38qemXP',
+    route: 'shop/hats',
+  },
+  {
+    id: 2,
+    title: 'jackets',
+    imageUrl: 'https://bit.ly/3wSaqZ3',
+    route: 'shop/jackets',
+  },
+  {
+    id: 3,
+    title: 'sneakers',
+    imageUrl: 'https://bit.ly/3NIThqj',
+    route: 'shop/sneakers',
+  },
+  {
+    id: 4,
+    title: 'womens',
+    imageUrl: 'https://bit.ly/3wSB4B4',
+    size: 'large',
+    route: 'shop/womens',
+  },
+  {
+    id: 5,
+    title: 'mens',
+    imageUrl: 'https://bit.ly/3lQkETz',
+    size: 'large',
+    route: 'shop/mens',
+  },
+];
+
+export default categories;
