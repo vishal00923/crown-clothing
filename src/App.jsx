616e86e51c5f6b8c4788d13141b6183b32d505b0
Vishal Chaurasia <vishalbg.mz@gmail.com>
2022-10-16 19:51:38 +0530

    refactoring

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import { Routes, Route } from 'react-router-dom';
 
 import Nav from './components/Nav/Nav';
@@ -7,7 +8,28 @@ import Shop from './routes/Shop/Shop';
 import Auth from './routes/Auth/Auth';
 import Checkout from './routes/Checkout/Checkout';
 
+import {
+  onAuthStateChangedListener,
+  createUserDocumentFromAuth,
+} from './utils/firebase';
+
+import { setCurrentUser } from './store/user/userAction';
+
 export default function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChangedListener((user) => {
+      if (user) {
+        createUserDocumentFromAuth(user);
+      }
+      dispatch(setCurrentUser(user));
+    });
+
+    return unsubscribe;
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <>
       <Nav />
--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
-import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
-import { UserProvider } from './contexts/userContext';
-import { CategoriesProvider } from './contexts/categoriesContext';
-import { CartProvider } from './contexts/cartContext';
+import { Provider } from 'react-redux';
+import { store } from './store/store';
 
 import App from './App';
 import './index.css';
@@ -13,15 +11,9 @@ const container = document.getElementById('root');
 const root = createRoot(container);
 
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <BrowserRouter>
-      <UserProvider>
-        <CategoriesProvider>
-          <CartProvider>
-            <App />
-          </CartProvider>
-        </CategoriesProvider>
-      </UserProvider>
+      <App />
     </BrowserRouter>
-  </React.StrictMode>
+  </Provider>
 );
